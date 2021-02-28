5fdeb54ef9fe72f63c1c84ac3fdfce81088e1cd3
Elizeu Matheus <elizeumatheus16@gmail.com>
2021-02-28 20:36:47 -0300

    feat: created home page structure

---
+++ src/pages/home/home.module.scss.d.ts
@@ -0,0 +1,4 @@
+export const container: string;
+export const content: string;
+export const image: string;
+export const title: string;
--- src/pages/home/index.tsx
+++ src/pages/home/index.tsx
@@ -1,3 +1,20 @@
-const Home = () => <div>home</div>
+import NoAuthLayout from 'layouts/NoAuthLayout'
+
+import { ReactComponent as FormsImage } from 'images/forms.svg'
+
+import * as styles from './home.module.scss'
+import SignIn from './components/SignIn'
+
+const Home = () => (
+  <NoAuthLayout className={styles.container}>
+    <picture className={styles.image}>
+      <FormsImage />
+    </picture>
+    <section className={styles.content}>
+      <h1 className={styles.title}>Create an account</h1>
+      <SignIn />
+    </section>
+  </NoAuthLayout>
+)
 
 export default Home
